71134aca69bb9c1bd07a673d0f85780c4c637bb6
brodie <brodiedog32@outlook.com>
2024-11-25 07:42:29 -0500

    Documentation
    Added some documentation for the user commands.

--- NexShell/NexShell/Commands/NexShellCommands.h
+++ NexShell/NexShell/Commands/NexShellCommands.h
@@ -1,6 +1,16 @@
 #ifndef NEXSHELL_COMMANDS_H
 	#define NEXSHELL_COMMANDS_H
 
+/** \file
+ * This is the interface to adding user commands.
+ * User commands are global commands that will appear in the help command list.
+ * There is a global \ref LINKED_LIST that is used to hold the data for the commands.
+ * When the shell gets a valid command it will go through this list to see if a 
+ * match occurs.  If there is a match the execute method will be called.
+ * 
+ * \image html UserCommandList.png "User Command List"
+ */
+
 #include "GenericTypes.h"
 #include "NexShell.h"
 
